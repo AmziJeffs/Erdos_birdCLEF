2d866581979c3372739ee4b23ac07f64fd6d29a0
AmziJeffs <amzijeffs0@gmail.com>
2024-05-17 14:04:21 -0400

    Update slicing function

--- Preprocessing/.ipynb_checkpoints/preprocessing-checkpoint.py
+++ Preprocessing/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -6,20 +6,32 @@
 #     - window_size, an integer
 #     - stride, an integer
 #     - align_left, a Boolean
+#     - return_scraps, a Boolean
 # Returns a list of segments in seq, each with length window_size,
 # consecutive segments offside by stride. If align_left is False,
 # these are shift right so that the last slice is "flush" with the 
 # righthand side of seq.
-# Our use case is when seq is an audio clip. 
-def slices(seq, window_size, stride = None, align_left = True):
-    # If one window covers the whole collection, just return the collection
-    if window_size >= len(seq):
-        return [seq]
+# If return_scraps is True, and we could have fit a partial window 
+# over the rest of our sequence, we return that partial window. If 
+# align_left is False, the "scrap" comes from the beginning of seq.
+# Our use case is when seq is an audio clip. For this reason, we default
+# the window size to 160000 = 5 seconds of samples at 32000 hz
+def slices(seq, window_size = 160000, stride = None, align_left = True, return_scraps = False):
+    
+    # If one window is larger than the sequence, just return the scraps or nothing
+    if window_size > len(seq):
+        if return_scraps == True:
+            return [seq]
+        else:
+            return []
 
-    indices = [i for i in range(len(seq))]
+    # If stride is None, it defaults to window_size
+    if stride == None:
+        stride = window_size
+    
     index_slices = []
     left_pointer = 0
-    while left_pointer + window_size < len(seq):
+    while left_pointer + window_size <= len(seq):
         index_slices += [[left_pointer, left_pointer + window_size]]
         left_pointer += stride
 
@@ -27,6 +39,12 @@ def slices(seq, window_size, stride = None, align_left = True):
         offset = len(seq)-(left_pointer-stride)-window_size
         index_slices = [[a+offset, b+offset] for [a,b] in index_slices]
 
+    if return_scraps == True and left_pointer + window_size > len(seq):
+        if align_left == True:
+            index_slices += [[left_pointer, len(seq)]]
+        else:
+            index_slices += [[0, len(seq) - left_pointer]]
+    
     return [seq[a:b] for [a,b] in index_slices]
 
     
