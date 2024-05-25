48d3018873dff50e7de36ce5a087fbdad8014c1e
AmziJeffs <amzijeffs0@gmail.com>
2024-05-25 18:22:58 -0400

    baseline python now supports saving checkpoints and training/validation losses

--- baseline.py
+++ baseline.py
@@ -1,3 +1,34 @@
+################################################################################
+# SETUP
+################################################################################
+
+# Convenience and saving flags
+ABRIDGED_RUN = False # Set to True to train and validate on 10% of the data, for quick funcitonality tests etc
+SAVE_AFTER_TRAINING = True # Save the model when you are done
+SAVE_CHECKPOINTS = True # Save the model after ever epoch
+REPORT_TRAINING_LOSS_PER_EPOCH = True # Track the training loss each epoch, and write it to a file after training
+REPORT_VALIDATION_LOSS_PER_EPOCH = True # Lets us make a nice learning curve after training
+
+# Training hyperparameters
+BATCH_SIZE = 256 # Number of samples per batch while training our network
+NUM_EPOCHS = 30 # Number of epochs to train our network
+LEARNING_RATE = 0.001 # Learning rate for our optimizer
+
+# Directories
+DATA_DIR = "data/"
+AUDIO_DIR = DATA_DIR + "train_audio/"
+CHECKPOINT_DIR = "checkpoints/" # Checkpoints, models, and training data will be saved here
+MODEL_NAME = None
+
+# Preprocessing info
+SAMPLE_RATE = 32000 # All our audio uses this sample rate
+SAMPLE_LENGTH = 5 # Duration we want to crop our audio to
+NUM_SPECIES = 182 # Number of bird species we need to label
+
+################################################################################
+# IMPORTS
+################################################################################
+
 # Basic imports
 import pandas as pd
 import numpy as np
@@ -9,9 +40,7 @@ import os
 import IPython.display as ipd
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-print("Basic imports done.")
-
-
+from pathlib import Path
 
 # Torch imports
 import torch
@@ -25,32 +54,11 @@ import torchvision.models as models
 from torchaudio.transforms import MelSpectrogram, Resample
 from IPython.display import Audio
 import torch.optim as optim
-print("Torch imports done.")
 
+################################################################################
+# LOAD DATA
+################################################################################
 
-
-# Directories
-DATA_DIR = "data/"
-AUDIO_DIR = DATA_DIR + "train_audio/"
-
-# Preprocessing info
-SAMPLE_RATE = 32000 # All our audio uses this sample rate
-SAMPLE_LENGTH = 5 # Duration we want to crop our audio to
-NUM_SPECIES = 182 # Number of bird species we need to label
-
-# Training hyperparameters
-BATCH_SIZE = 256 # Number of samples per batch while training our network
-NUM_EPOCHS = 50 # Number of epochs to train our network
-LEARNING_RATE = 0.001 # Learning rate for our optimizer
-
-# Convenience and saving
-ABRIDGED_TRAINING = False # Set to True to train on 10% of the training data, for quick funcitonality tests etc
-SAVE_AFTER_TRAINING = True
-
-
-
-
-# Load data
 data = pd.read_csv(DATA_DIR+"train_metadata.csv")
 data['filepath'] = AUDIO_DIR + data['filename']
 
@@ -61,20 +69,19 @@ data = data[['filepath', 'primary_label']]
 species = data['primary_label'].unique()
 species_to_index = {species[i]:i for i in range(len(species))}
 data['tensor_label'] = pd.Series(pd.get_dummies(data['primary_label']).astype(int).values.tolist()).apply(lambda x: torch.Tensor(x))
-print("Loaded data")
-
-
+data.sample(5)
 
 # Train test split, stratified by species
 data_train, data_test = train_test_split(data, test_size = 0.2, stratify=data['primary_label'])
 
-if ABRIDGED_TRAINING == True:
+# Use 10% of data for quick runs to test compile/functionality
+if ABRIDGED_RUN == True:
     data_train = data_train.sample(int(len(data_train)*0.1))
+    data_test = data_test.sample(int(len(data_train)*0.1))
 
-print(data_train.info())
-
-
-
+################################################################################
+# PREPROCESSING FUNCTIONS
+################################################################################
 
 # Transforms audio signal to a spectrogram
 spectrogram_transform = torchaudio.transforms.Spectrogram(
@@ -99,6 +106,14 @@ db_scaler = torchaudio.transforms.AmplitudeToDB(stype="power", top_db=80)
 # Resizes spectrograms into square images
 resize = transforms.Resize((224, 224), antialias = None)
 
+# Processes a sample to a tensor for our network
+def sample_to_tensor(sample):
+    x = spectrogram_transform(sample)
+    x = mel_spectrogram_transform(x)
+    x = db_scaler(x)
+    x = resize(x)
+    return x
+
 # Takes a filepath and outputs a torch tensor with shape (1, 224, 224) 
 # that we can feed into our CNN
 def filepath_to_tensor(filepath):
@@ -108,18 +123,14 @@ def filepath_to_tensor(filepath):
     else:
         pad_length = SAMPLE_RATE * SAMPLE_LENGTH - len(sample)
         sample = torch.nn.functional.pad(sample, (0, pad_length))
-    spec = spectrogram_transform(sample)
-    mel_spec = mel_spectrogram_transform(spec)
-    db_scaled_mel_spec = db_scaler(mel_spec)
-    resized = resize(db_scaled_mel_spec)
-    return resized
+    return sample_to_tensor(sample)
 
-
-
-
+################################################################################
+# DATASET CLASS
+################################################################################
 
 # Note: filepaths and labels should be ordinary lists
-class birdCLEF_dataset(Dataset):
+class BirdDataset(Dataset):
     def __init__(self, filepaths, labels):
         super().__init__()
         self.filepaths = filepaths
@@ -132,13 +143,14 @@ class birdCLEF_dataset(Dataset):
         processed_clip = filepath_to_tensor(self.filepaths[index])
         return processed_clip, self.labels[index]
 
+################################################################################
+# ARCHITECTURE
+################################################################################
 
-
-
-class birdClassifier(nn.Module):
+class BirdClassifier(nn.Module):
     ''' Pared down architecture from https://github.com/musikalkemist/pytorchforaudio/blob/main/10%20Predictions%20with%20sound%20classifier/cnn.py'''
     def __init__(self, num_classes):
-        super(birdClassifier, self).__init__()
+        super(BirdClassifier, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(
                 in_channels=1,
@@ -170,54 +182,74 @@ class birdClassifier(nn.Module):
         x = self.conv2(x)
         x = self.dropout(x)
         x = self.flatten(x)
-        logits = self.linear(x)
-        return logits
+        x = self.linear(x)
+        return x
 
+################################################################################
+# TRAINING SETUP
+################################################################################
 
-
-
-
-
- # Set device we'll train on
+# Set device we'll train on
 device = None
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
-print(f"Training with {device}")
+print(f"Using {device}")
 
-# Instantiate our dataset
-train_dataset = birdCLEF_dataset(filepaths = data_train['filepath'].to_list(), labels = data_train['tensor_label'].to_list())
+# Set model name if not set.
+# Defaults to a timestamp, YYYY-MM-DD_HH_MM_SS
+if MODEL_NAME == None:
+    MODEL_NAME = str(pd.Timestamp.now()).replace(" ", "_").replace(":", "-").split(".")[0]
+
+# Create a saving directory if needed
+if SAVE_AFTER_TRAINING or SAVE_CHECKPOINTS or REPORT_TRAINING_LOSS_PER_EPOCH or REPORT_VALIDATION_LOSS_PER_EPOCH:
+    output_dir = Path(f'{CHECKPOINT_DIR}{MODEL_NAME}')
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+# Instantiate our training dataset
+train_dataset = BirdDataset(filepaths = data_train['filepath'].to_list(), labels = data_train['tensor_label'].to_list())
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-# Instantiate our model
-model = birdClassifier(NUM_SPECIES).to(device)
+# Instantiate our validation dataset
+validation_dataset =  BirdDataset(filepaths = data_test['filepath'].to_list(), labels = data_test['tensor_label'].to_list())
+validation_dataloader = DataLoader(validation_dataset, batch_size=1, shuffle=False)
 
+# Instantiate our model
+model = BirdClassifier(NUM_SPECIES).to(device)
 
 # Set our loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9)
 
 # Training loop
-print_per_n_batches = 10
 print(f"Training on {len(train_dataset)} samples with {BATCH_SIZE} samples per batch.")
+if REPORT_VALIDATION_LOSS_PER_EPOCH == True:
+    print(f"Validating on {len(validation_dataset)} samples at the end of each epoch.")
+
+training_losses = [None]*NUM_EPOCHS
+validation_losses = [None]*NUM_EPOCHS
 
 torch.enable_grad() # Turn on the gradient
 
-training_losses = [0]*NUM_EPOCHS
+################################################################################
+# TRAINING LOOP
+################################################################################
 
-for epoch in tqdm(range(NUM_EPOCHS)):  # loop over the dataset multiple times
+for epoch_num, epoch in enumerate(tqdm(range(NUM_EPOCHS))):
 
     running_loss = 0.0
-    for i, data in enumerate(tqdm(train_dataloader, leave=False)):
-        # get the inputs; data is a list of [inputs, labels]
+    
+    for i, data in enumerate(tqdm(train_dataloader, leave = False)):
+        
+        # Get batch of inputs and true labels, push to device
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
 
-        # zero the parameter gradients
+        # Zero gradients
         optimizer.zero_grad()
 
-        # forward + backward + optimize
+        # Forward pass on batch of inputs
         outputs = model(inputs)
 
         # Compute the loss and its gradients
@@ -227,39 +259,45 @@ for epoch in tqdm(range(NUM_EPOCHS)):  # loop over the dataset multiple times
         # Update weights
         optimizer.step()
 
-        # print statistics
+        # Update loss
         running_loss += loss.item()
-        if i % print_per_n_batches == print_per_n_batches-1:    # print after some batches
-            print(f'[{epoch + 1}/{NUM_EPOCHS}, {(i + 1):5d}/{len(train_dataloader)}] loss: {running_loss / print_per_n_batches:.3f}')
-            running_loss = 0.0
-    training_losses[epoch] = running_loss / len(train_dataloader)
+
+    # Save checkpoint
+    if SAVE_CHECKPOINTS == True:
+        torch.save(model.state_dict(), f"{CHECKPOINT_DIR}{MODEL_NAME}/checkpoint_{epoch_num+1}.pt")
+
+    # Compute training loss
+    if REPORT_TRAINING_LOSS_PER_EPOCH == True:    
+        training_losses[epoch_num] = running_loss/len(train_dataloader)
+        
+    # Compute validation loss
+    if REPORT_VALIDATION_LOSS_PER_EPOCH == True:
+        validation_loss = 0.0
+        model.eval()
+        for validation_data in validation_dataloader:
+            inputs, labels = validation_data
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            validation_loss += criterion(outputs, labels).item()
+        validation_losses[epoch_num] = validation_loss/len(validation_dataloader)
+        model.train()
+
 print('Finished Training')
-print(training_losses)
 
-
-
-
-
+################################################################################
+# SAVE AND REPORT
+################################################################################
 
 if SAVE_AFTER_TRAINING == True:
-    torch.save(model.state_dict(), "".join(str(pd.Timestamp.now()).split())+"_NUM_EPOCHS_"+str(NUM_EPOCHS)+".pt")
+    torch.save(model.state_dict(), f"{CHECKPOINT_DIR}{MODEL_NAME}/final.pt")
 
+losses = pd.DataFrame({"training_losses":training_losses, "validation_losses":validation_losses})
+cols = []
+if REPORT_TRAINING_LOSS_PER_EPOCH == True:
+    cols += ["training_losses"]
+if REPORT_VALIDATION_LOSS_PER_EPOCH == True:
+    cols += ["validation_losses"]
+if len(cols) > 0:
+    losses[cols].to_csv(f"{CHECKPOINT_DIR}{MODEL_NAME}/losses.csv")
+    print(losses)
 
-print("Starting validation")
-test_dataset =  birdCLEF_dataset(filepaths = data_test['filepath'].to_list(), labels = data_test['tensor_label'].to_list())
-test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-torch.no_grad() # Turn off gradient for fast validation loss computations
-model.eval()
-
-losses = []
-for i, data in enumerate(test_dataloader):
-    inputs, labels = data
-    inputs, labels = inputs.to(device), labels.to(device)
-
-    pred = model(inputs)
-    losses += [float(criterion(pred, labels))]
-losses = pd.Series(losses)
-losses.index = data_test.index
-data_test['loss'] = losses
-print(f"Average loss on test data: {data_test['loss'].sum() / len(data_test['loss'])}")
